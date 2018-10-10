51161879ba6fcafd0d89a79b7a949c3f825cb49f
hyb-bobo <31762573+hyb-bobo@users.noreply.github.com>
2018-10-10 15:31:54 +0800

    xczx

---
+++ xc-service-api/src/main/java/com/xuecheng/api/CmsPageControllerApi.java
@@ -0,0 +1,12 @@
+package com.xuecheng.api;
+
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+
+/**
+ * Created by bobo on 2018/10/10.
+ */
+public interface CmsPageControllerApi {
+
+    public QueryResponseResult findList(int page,int size,QueryPageRequest queryPageRequest);
+}
