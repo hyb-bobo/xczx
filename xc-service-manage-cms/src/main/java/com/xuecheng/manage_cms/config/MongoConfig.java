764215e09b23aca7204ef3cb1cbef12c589f3b10
hyb-bobo <31762573+hyb-bobo@users.noreply.github.com>
2018-11-14 09:24:20 +0800

    xczx-freemarker test

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/config/MongoConfig.java
@@ -0,0 +1,26 @@
+package com.xuecheng.manage_cms.config;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by bobo on 2018/10/25.
+ */
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.database}")
+//    @Value("${spring.data.mongodb.uri}")
+    String db;
+    @Bean
+    public GridFSBucket getGridFSBucket(MongoClient mongoClient){
+        MongoDatabase database = mongoClient.getDatabase(db);
+        GridFSBucket bucket = GridFSBuckets.create(database);
+        return bucket;
+    }
+}
