8a4e4adf996fcf43b6cf1c8f1e12be98a0062caf
hyb-bobo <31762573+hyb-bobo@users.noreply.github.com>
2018-12-13 15:45:18 +0800

    xczx--springboot mq

---
+++ test-rabbitmq-consumer/src/main/java/com/xuecheng/consumer/ReceiveHandler.java
@@ -0,0 +1,26 @@
+package com.xuecheng.consumer;
+
+import com.xuecheng.test.rabbitmq.config.RabbitmqConfig;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by bobo on 2018/12/13.
+ */
+@Component
+public class ReceiveHandler {
+
+    //监听email队列
+    @RabbitListener(queues = {RabbitmqConfig.QUEUE_INFORM_EMAIL})
+    public void receive_email(String msg, Message message, Channel channel) {
+        System.out.println(msg);
+    }
+
+    //监听sms队列
+    @RabbitListener(queues = {RabbitmqConfig.QUEUE_INFORM_SMS})
+    public void receive_sms(String msg, Message message, Channel channel) {
+        System.out.println(msg);
+    }
+}
---
+++ test-rabbitmq-producer/src/main/java/com/xuecheng/TestMq.java
@@ -0,0 +1,14 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by bobo on 2018/12/13.
+ */
+@SpringBootApplication
+public class TestMq {
+    public static void main(String[] args) {
+        SpringApplication.run(TestMq.class,args);
+    }
+}
