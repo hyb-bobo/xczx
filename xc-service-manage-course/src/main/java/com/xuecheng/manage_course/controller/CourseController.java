0e622b304e5c0a2546f3bba23833215f5e006405
hyb-bobo <31762573+hyb-bobo@users.noreply.github.com>
2018-12-29 09:42:15 +0800

    添加课程计划

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/controller/CourseController.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/controller/CourseController.java
@@ -1,12 +1,12 @@
 package com.xuecheng.manage_course.controller;
 
 import com.xuecheng.api.course.CourseControllerApi;
+import com.xuecheng.framework.domain.course.Teachplan;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.manage_course.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by bobo on 2018/12/27.
@@ -22,7 +22,14 @@ public class CourseController implements CourseControllerApi {
     //查询课程计划
     @Override
     @GetMapping("/teachplan/list/{courseId}")
-    public TeachplanNode findTeachplanList(String courseId) {
+    public TeachplanNode findTeachplanList(@PathVariable("courseId") String courseId) {
         return courseService.findTeachplanList(courseId);
+    }
+
+    //添加课程计划
+    @Override
+    @PostMapping("/teachplan/add")
+    public ResponseResult addTeachplan(@RequestBody Teachplan teachplan) {
+        return courseService.addTeachplan(teachplan);
     }
 }
